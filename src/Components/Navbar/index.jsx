1d3e03ea96acdf79dd39fd0412bb5a580d7b3c83
bayu <bayusetiawanipb@gmail.com>
2023-05-19 21:01:46 +0700

    fix scroll offset

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,30 +1,29 @@
 import { Link } from 'react-scroll';
-import { Text, HStack, Button } from '@chakra-ui/react';
+import { Text, HStack } from '@chakra-ui/react';
 import DownloadCV from '../DownloadCV';
 import { navbarStyles as sx } from './Navbar.styles';
-import { NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
     <HStack data-testid='navbar' sx={sx.container}>
       <HStack sx={sx.wrap} spacing={{ base: '5', lg: '10' }}>
-        <Link href='/' to='home' spy={true} smooth={true} offset={-50} duration={500}>
+        <Link href='/' to='home' spy={true} smooth={true} offset={0} duration={500}>
           <Text sx={sx.link}>Home</Text>
         </Link>
         <Link
           to='experience'
           spy={true}
           smooth={true}
-          offset={-50}
+          offset={0}
           duration={500}
           href='/'
         >
           <Text sx={sx.link}>Experience</Text>
         </Link>
-        <Link href='/' to='mystack' spy={true} smooth={true} offset={-50} duration={500}>
+        <Link href='/' to='mystack' spy={true} smooth={true} offset={0} duration={500}>
           <Text sx={sx.link}>Tech Stack</Text>
         </Link>
-        <Link href='/' to='projects' spy={true} smooth={true} offset={-50} duration={500}>
+        <Link href='/' to='projects' spy={true} smooth={true} offset={0} duration={500}>
           <Text sx={sx.link}>Projects</Text>
         </Link>
       </HStack>
